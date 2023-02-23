fc43f83a6350edc3ecba480e191a1893635085fc
Khai Nguyen Quoc <nquockhai@tma.com.vn>
2023-02-23 13:27:36 +0700

    update

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write(`{ "type": "message", "text": "This is a reply!" }`); //write a response to the client
   res.end(); //end the response
-}).listen(3000); //the server object listens on port 8080
+}).listen(3000, () => Ơ
+console.log("server is listening")
+); //the server object listens on port 8080
