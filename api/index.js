a7132cb51a1ea3d04713f48dbe85bdb58497c8ab
Khai Nguyen Quoc <nquockhai@tma.com.vn>
2023-02-28 09:15:26 +0700

    update

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.post('/api', (req, res) => {
-  console.log(req.body)
+  console.log(JSON.stringify(req.body, null, 4))
   res.json({
     type: "message",
     text: JSON.stringify(req.body)
