d8be9772c730008558bd643d3314ba776401baac
Khai Nguyen Quoc <nquockhai@tma.com.vn>
2023-03-02 13:47:57 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,28 +1,13 @@
 const express = require('express')
 const app = express();
-const tableify = require('tableify');
-
-const jsonStr = '[{"Name":"Alice","Age":30,"Address":"123 Main St"},{"Name":"Bob","Age":25,"Address":"456 Elm St"}]';
-const people = JSON.parse(jsonStr);
-
-const tableHtml = tableify(people, {
-  headers: {
-    Name: 'Name',
-    Age: 'Age',
-    Address: 'Address'
-  },
-  class: 'my-table'
-});
-
-console.log(tableHtml);
 
 app.use(express.json());
 
 app.post('/api', (req, res) => {
-  console.log(req.body)
+  console.log(JSON.stringify(req.body, null, 4))
   res.json({
     type: "message",
-    text: tableHtml 
+    text: JSON.stringify(req.body) 
   })
 });
 
